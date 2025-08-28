dd6d20660d6439c642483fdaf3b1c44b9697b632
Nicolas Moreno Bejarano <nicolasmorenobejarano@192.168.0.4>
2025-08-27 20:06:51 -0500

    Add test endpoint to verify Netlify Functions

---
+++ netlify/functions/test.js
@@ -0,0 +1,14 @@
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      success: true,
+      message: "Test endpoint working",
+      timestamp: new Date().toISOString()
+    })
+  };
+};
